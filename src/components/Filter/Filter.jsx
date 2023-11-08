5c622114c8823d336a4c349ed59d4afd6fbe2239
alivaliev <aliveliyev@hotmail.com>
2023-11-08 12:14:52 +0400

    bug fixed

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,26 +2,41 @@ import React, { useEffect, useState } from "react";
 import "./Filter.scss";
 
 export default function Filter() {
-  const url =
-    "https://assignment-6fdaf-default-rtdb.firebaseio.com/orders.json";
+  const url = "https://assignment-6fdaf-default-rtdb.firebaseio.com/orders.json"; 
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
   const [amountSearch, setAmountSearch] = useState("");
   const [dateSearch, setDateSearch] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
 
-const filteredData = () => {
-
-}
-const getData = () => {
-  return fetch(url)
-    .then((x) => x.json())
-    .then((x) => setData(x));
-};
-
+  const getData = () => {
+    fetch(url)
+      .then((x) => x.json())
+      .then((a) => {
+        setData(a);
+        setFilteredData(a); 
+      })
+  };
 
   useEffect(() => {
     getData();
   }, []);
+
+  const handleFilterClick = () => {
+    const filteredItems = data.filter((item) => {
+      const productName = item.product[0].name.toLowerCase();
+      const orderDate = new Date(item.date).toISOString().split("T")[0];
+      const orderAmount = item.amount.toString();
+
+      const nameMatch = productName.includes(search.toLowerCase());
+      const amountMatch = orderAmount.includes(amountSearch);
+      const dateMatch = orderDate.includes(dateSearch);
+
+      return nameMatch && amountMatch && dateMatch;
+    });
+
+    setFilteredData(filteredItems);
+  };
 
   return (
     <div>
@@ -42,7 +57,7 @@ const getData = () => {
           type="date"
           placeholder="Select Date"
         />
-        <button>Filter</button>
+        <button onClick={handleFilterClick}>Filter</button>
       </div>
       <div className="table">
         <table>
@@ -52,32 +67,14 @@ const getData = () => {
             <th>Order Amount</th>
             <th>Total Price</th>
           </tr>
-          {data
-            .filter((item) => {
-              const productName = item.product[0].name.toLowerCase();
-              const orderDate = new Date(item.date)
-                .toISOString()
-                .split("T")[0];
-              const orderAmount = item.amount.toString();
-
-              const nameMatch = productName.includes(search.toLowerCase());
-              const amountMatch = orderAmount.includes(amountSearch);
-              const dateMatch = orderDate.includes(dateSearch);
-
-              return (
-                nameMatch &&
-                amountMatch &&
-                dateMatch
-              );
-            })
-            .map((item) => (
-              <tr key={item._id}>
-                <td>{item.product[0].name}</td>
-                <td>{new Date(item.date).toISOString().split("T")[0]}</td>
-                <td>{item.amount}</td>
-                <td>${item.product[0].price}</td>
-              </tr>
-            ))}
+          {filteredData.map((item) => (
+            <tr key={item._id}>
+              <td>{item.product[0].name}</td>
+              <td>{new Date(item.date).toISOString().split("T")[0]}</td>
+              <td>{item.amount}</td>
+              <td>${item.product[0].price}</td>
+            </tr>
+          ))}
         </table>
       </div>
     </div>
